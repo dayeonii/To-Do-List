1b98f4b8ce1866f47f41db1aa448cfc028fda356
dayeonii <daabbong@gmail.com>
2023-08-11 20:57:54 +0900

    todo 페이지 세팅 및 todo 추가 기능 구현

---
+++ todo.js
@@ -0,0 +1,28 @@
+const toDoForm = document.querySelector('#todo-form');
+const toDoInput = document.querySelector('#todo-form input');
+const toDoList = document.querySelector('#todo-list');
+
+function deleteToDo() {
+    console.log('delete');
+}
+
+function paintToDo(newTodo) {   //입력값 표시하기
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const button = document.createElement('button');
+    span.innerText = newTodo;
+    button.innerText = '🗑'
+    button.addEventListener('click', deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault(); //새로고침 되는거 막아줌
+    const newTodo = toDoInput.value;    //입력값 가져오기
+    toDoInput.value = "";   //입력값 초기화 해주기
+    paintToDo(newTodo);
+}
+
+toDoForm.addEventListener('submit', handleToDoSubmit);
